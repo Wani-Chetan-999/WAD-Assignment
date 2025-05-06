380ebebc6a3a4f6a2f577ae5d2482396a5746d8d
Chetan Wani <02ckwani@gmail.com>
2025-05-06 08:07:52 +0530

    added angular

---
+++ DockerPrac/index.js
@@ -0,0 +1,7 @@
+// Example of printing data using console.log
+
+console.log("Hello, World!"); // Prints a string
+console.log(42); // Prints a number
+console.log(true); // Prints a boolean
+console.log({ name: "John", age: 30 }); // Prints an object
+console.log([1, 2, 3, 4, 5]); // Prints an array
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # WAD-Assignment
 Web Application Development 
+https://github.com/Rugz007/WAD-Assignments/tree/master
+
+ShreeSS@9999Sss@9999
